f147523b6dacc79ee78646bc4c0c0e8315d6a445
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-19 14:17:34 -0300

    Adicionado novas melhorias

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -11,26 +11,53 @@ class Program
     static void Main(string[] args)
     {
 
+
+        const int limiteLinhaChegada = 30;
+
         while (true)
         {
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine("Jogo dos Dados");
-            Console.WriteLine("----------------------------------");
+
+            int posicaoJogador = 0;
+            bool jogoEstaEmAndamento = true;
+
+            while (jogoEstaEmAndamento)
+            {
+                Console.Clear();
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Jogo dos Dados");
+                Console.WriteLine("----------------------------------");
 
 
-            // logica do jogo
-            Console.Write("Pressione ENTER para lancar um dado...");
-            Console.ReadLine();
+                // logica do jogo
+                Console.Write("Pressione ENTER para lancar um dado...");
+                Console.ReadLine();
 
 
-            int resultado = RandomNumberGenerator.GetInt32(1, 7);
+                int resultado = RandomNumberGenerator.GetInt32(1, 7);
 
 
-            Console.WriteLine("----------------------------------");
-            Console.WriteLine($"O numero sorteado foi: {resultado}");
-            Console.WriteLine("----------------------------------");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine($"O numero sorteado foi: {resultado}");
+                Console.WriteLine("----------------------------------");
+
+                posicaoJogador += resultado;
+
+                if (posicaoJogador < limiteLinhaChegada)
+                    Console.WriteLine($"Voce esta na posicao: {posicaoJogador} de {limiteLinhaChegada}");
 
 
+                else
+                {
+                    Console.WriteLine("Parabens! Voce alcançou a linha de chegada.");
+
+                    jogoEstaEmAndamento = false;
+                }
+
+                    Console.WriteLine("Pressione ENTER para continuar..");
+                Console.ReadLine();
+
+
+            }
 
             Console.WriteLine("Deseja continuar? (s/n): ");
             string? opcaoContinuar = Console.ReadLine()?.ToUpper();
